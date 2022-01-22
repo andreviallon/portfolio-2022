9c4738d98d9bea01ef29e29b017f219a0db1523e
Andre Viallon <viallon.andre@gmail.com>
2022-01-22 14:01:08 +0100

    chore: clean up header comp

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -8,54 +8,40 @@
 
 <header class="fixed top-0 left-0 right-0 z-20">
 	<div class="flex py-8 px-10 justify-between items-center">
-		<a class="font-medium text-xl bottom-bar-on-hover cursor-pointer bottom-bar-on-hover" href="/">
-			andré viallon
-		</a>
+		<a class="text-xl bottom-bar-on-hover" href="/"> andré viallon </a>
 		<div class="hidden sm:flex">
-			<a
-				class="font-medium text-lg bottom-bar-on-hover cursor-pointer"
-				href="/"
-				on:click={toggleMenu}
-			>
-				portfolio
-			</a>
-			<a
-				class="font-medium text-lg ml-10 bottom-bar-on-hover cursor-pointer"
-				href="/"
-				on:click={toggleMenu}
-			>
-				about
-			</a>
-			<a
-				href="/"
-				target="_blank"
-				rel="noreferrer"
-				class="font-medium text-lg ml-10 bottom-bar-on-hover cursor-pointer"
-			>
-				resume
-			</a>
+			<a href="/" class="bottom-bar-on-hover ml-10" on:click={toggleMenu}> projects </a>
+			<a href="/" class="bottom-bar-on-hover ml-10" on:click={toggleMenu}> about </a>
+			<a href="/" class="bottom-bar-on-hover ml-10" target="_blank" rel="noreferrer"> resume </a>
 		</div>
 
 		<div class="flex sm:hidden cursor-pointer">
 			{#if isMenuOpen}
-				<span class="font-medium" on:click={toggleMenu}>close</span>
+				<span on:click={toggleMenu}>close</span>
 			{:else}
-				<span class="font-medium" on:click={toggleMenu}>menu</span>
+				<span on:click={toggleMenu}>menu</span>
 			{/if}
 		</div>
 	</div>
-
-	{#if isMenuOpen}
-		<div
-			class="fixed top-0 right-0 left-0 bottom-0 bg-gray-200 sm:hidden flex justify-center items-center z-10"
-		>
-			<div class="flex flex-col items-center mt-12">
-				<a class="font-medium text-2xl mb-10" href="/" on:click={toggleMenu}> portfolio </a>
-				<a class="font-medium text-2xl mb-10" href="/" on:click={toggleMenu}> about </a>
-				<a href={'resume.pdf'} target="_blank" rel="noreferrer" class="font-medium text-2xl mb-10">
-					resume
-				</a>
-			</div>
-		</div>
-	{/if}
 </header>
+{#if isMenuOpen}
+	<div
+		class="fixed top-0 right-0 left-0 bottom-0 bg-gray-200 sm:hidden flex justify-center items-center z-10"
+	>
+		<div class="flex flex-col items-center mt-12">
+			<a class="text-2xl mb-10" href="/" on:click={toggleMenu}> projects </a>
+			<a class="text-2xl mb-10" href="/" on:click={toggleMenu}> about </a>
+			<a href={'resume.pdf'} target="_blank" rel="noreferrer" class="text-2xl mb-10"> resume </a>
+		</div>
+	</div>
+{/if}
+
+<style>
+	a {
+		@apply font-medium text-lg cursor-pointer;
+	}
+
+	span {
+		@apply font-medium;
+	}
+</style>
