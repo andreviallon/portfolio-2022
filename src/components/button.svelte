67c7579fdfbdf93763b435df2c7038d7b00bf00f
Andre Viallon <viallon.andre@gmail.com>
2022-01-22 14:00:19 +0100

    chore: button comp

---
+++ src/components/button.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let text = '';
+</script>
+
+<button
+	class="
+    py-4
+    px-6
+    rounded-md
+    text-white
+    font-medium
+    shadow-lg
+    shadow-indigo-300
+
+    bg-gradient-to-r
+    from-indigo-500
+    to-indigo-600
+    
+    transition
+    ease-in-out
+    delay-100
+    duration-300
+
+    hover:-translate-y-1
+    hover:shadow-indigo-500"
+>
+	{text}
+</button>
