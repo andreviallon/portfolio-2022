81ef6567ac69086640427eb96f542c1945d4ce94
Andre Viallon <viallon.andre@gmail.com>
2022-01-22 14:01:29 +0100

    chore: add header and hero to home comp

--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -1,7 +1,16 @@
 <script>
 	import Header from '../components/header.svelte';
+	import Hero from '../components/hero.svelte';
+	import Avatar from '../components/avatar.svelte';
 </script>
 
-<div>
+<div class="h-full">
 	<Header />
+	<div class="container mx-auto px-8 h-full">
+		<div class="flex h-full">
+			<div class="flex w-full xl:w-1/2">
+				<Hero />
+			</div>
+		</div>
+	</div>
 </div>
