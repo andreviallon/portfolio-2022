ba301ae516e1e83a189cbc7f70a7e8bb8ed73e27
Andre Viallon <viallon.andre@gmail.com>
2022-01-22 17:37:41 +0100

    chore: project model

---
+++ src/model/project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+	image: string;
+	title: string;
+	description: string;
+	frontendStack: string[];
+	backendStack: string[];
+	websiteLink: string;
+	githubLink: string;
+}
