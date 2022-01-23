741ed08e2ba4375ab2d890f15c6736f4c5a10cea
Andre Viallon <viallon.andre@gmail.com>
2022-01-23 08:31:14 +0100

    chore: make stacks optional

--- src/model/project.ts
+++ src/model/project.ts
@@ -2,8 +2,8 @@ export interface Project {
 	image: string;
 	title: string;
 	description: string;
-	frontendStack: string[];
-	backendStack: string[];
+	frontendStack?: string[];
+	backendStack?: string[];
 	websiteLink: string;
 	githubLink: string;
 }
