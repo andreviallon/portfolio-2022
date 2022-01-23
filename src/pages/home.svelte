3d23f4de468f3874e28497095c800b01f4358f04
Andre Viallon <viallon.andre@gmail.com>
2022-01-23 16:25:14 +0100

    chore: register about comp

--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -3,12 +3,16 @@
 	import Hero from '../components/hero.svelte';
 	import Avatar from '../components/avatar.svelte';
 	import Projects from '../components/projects.svelte';
+	import About from '../components/about.svelte';
+	import avatar from '../../static/avatar.png';
+
+	const year = new Date().getFullYear();
 </script>
 
 <div class="h-full">
 	<Header />
 	<div class="container mx-auto px-8 h-full">
-		<div class="flex h-full">
+		<div class="flex hero-container">
 			<div class="flex w-full xl:w-1/2">
 				<Hero />
 			</div>
@@ -17,5 +21,17 @@
 			</div>
 		</div>
 		<Projects />
+		<div class="mt-32">
+			<About />
+		</div>
+		<p class="mt-48 mb-24 flex justify-center w-full">
+			©{year} Andre viallon, all rights reserved.
+		</p>
 	</div>
 </div>
+
+<style lang="scss">
+	.hero-container {
+		height: 100vh;
+	}
+</style>
