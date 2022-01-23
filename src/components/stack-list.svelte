cf7630379945364d6bfbbb69e0b83148a68912a3
Andre Viallon <viallon.andre@gmail.com>
2022-01-23 08:33:14 +0100

    feat: stack list comp

---
+++ src/components/stack-list.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { IconDefinition } from '@fortawesome/fontawesome-common-types';
+	import Fa from 'svelte-fa';
+
+	export let text: string;
+	export let icon: IconDefinition;
+	export let stackList: string[];
+	export let isHovering: boolean;
+
+	const isLast = (array: string[], index: number): boolean => array.length !== index + 1;
+</script>
+
+<div class="stack-list flex items-center {isHovering ? 'text-slate-200' : 'text-slate-500'}">
+	<div class="flex justify-center w-8">
+		<Fa class="" {icon} />
+	</div>
+	<span class="">{text}:</span>
+	<ul class="ml-1">
+		{#each stackList as stackItem, index}
+			<span>{stackItem}{isLast(stackList, index) ? ', ' : ''} </span>
+		{/each}
+	</ul>
+</div>
